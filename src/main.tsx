a8b61d9c00c5b9fe9f4e01e5f89746c0ca56c3d3
Shannon Rothe <shannon.michael.rothe@gmail.com>
2022-03-19 13:05:00 +1100

    border-box

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+import { globalCss } from '@stitches/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Toastful } from 'react-toastful';
 import App from './App';
 
+const g = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    {g()}
     <Toastful defaultStyle={true} />
     <App />
   </React.StrictMode>,
